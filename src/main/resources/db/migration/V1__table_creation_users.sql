db2f82b9826b85a3e4bc65ee24ead096aa6b0a41
Tamas Oravecz <oravecztamas@gmail.com>
2024-05-05 09:17:42 +0200

    Added FlyWay dependencies, app.cproperties and created flyway script

--- src/main/resources/db/migration/V1__table_creation_users.sql
+++ src/main/resources/db/migration/V1__table_creation_users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
+    username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
