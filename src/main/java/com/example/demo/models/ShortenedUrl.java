2cdca9a5e345f3e15cf326b99acc6bf968007687
Sandor Felgyinszky <sandor.felgyinszky@outlook.com>
2024-05-07 18:29:40 +0200

    database creation fixed

--- src/main/java/com/example/demo/models/ShortenedUrl.java
+++ src/main/java/com/example/demo/models/ShortenedUrl.java
@@ -1,9 +1,6 @@
 package com.example.demo.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 @Getter
 @Setter
 @Entity
+@Table(name="urls")
 public class ShortenedUrl {
 
  private String url;
