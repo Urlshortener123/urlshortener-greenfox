f98a40375b5790c275374d253a8fe6eadf4fd66c
Tamas Oravecz <oravecztamas@gmail.com>
2024-05-22 22:03:08 +0200

    Upgraded 1 dependency, renamed LinkServices to LinkService, changed @Mock and @Autowired annotations for tests

--- src/main/java/com/example/demo/services/LinkService.java
+++ src/main/java/com/example/demo/services/LinkService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class LinkServices {
+public class LinkService {
 
     private final LinkRepository linkRepository;
 
--- src/test/java/com/example/demo/UrlshortenerGreenfoxApplicationTests.java
+++ src/test/java/com/example/demo/UrlshortenerGreenfoxApplicationTests.java
@@ -1,42 +1,33 @@
 package com.example.demo;
 
-import com.example.demo.controllers.UrlController;
 import com.example.demo.models.ShortenedUrl;
 import com.example.demo.repositories.LinkRepository;
-import com.example.demo.services.LinkServices;
-import org.junit.jupiter.api.BeforeAll;
+import com.example.demo.services.LinkService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Optional;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 class UrlshortenerGreenfoxApplicationTests {
+
     @Test
     void contextLoads() {
     }
 
     @Autowired
-    private LinkServices service;
-    @MockBean
+    private LinkService service;
+    @Autowired
     private LinkRepository linkRepository;
     @Autowired
     private MockMvc mockMvc;
@@ -45,8 +36,6 @@ class UrlshortenerGreenfoxApplicationTests {
     public void setUp() {
         ShortenedUrl shortenedUrl = new ShortenedUrl("http://testingurl.com", "testuuid", 1L);
         linkRepository.save(shortenedUrl);
-        //if we comment out  the line below  the last test will fail, h2 database is not working
-        Mockito.when(linkRepository.findByUuid("testuuid")).thenReturn(shortenedUrl);
     }
 
     @Test
@@ -60,6 +49,5 @@ class UrlshortenerGreenfoxApplicationTests {
     public void getLinkTestFromUrl() throws Exception {
         this.mockMvc.perform(get("/r/testuuid")).andExpect((redirectedUrl(linkRepository.findByUuid("testuuid").getUrl())));
     }
-
 
 }
