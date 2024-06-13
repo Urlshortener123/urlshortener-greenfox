76a1a0f7a9abfcd4992717658dfb338962f37667
Tamas Oravecz <oravecztamas@gmail.com>
2024-06-13 22:31:40 +0200

    Fixed conflicts in tests and migration scripts

--- src/main/resources/db/migration/V3__modify_table_urls.sql
+++ src/main/resources/db/migration/V3__modify_table_urls.sql
@@ -9,3 +9,17 @@ ALTER TABLE urls
 
 ALTER TABLE urls
     ADD click_count INTEGER DEFAULT 0;
+
+ALTER TABLE users
+    ADD email VARCHAR(255) NOT NULL;
+
+ALTER TABLE users
+    ADD email_verified BOOLEAN DEFAULT FALSE;
+
+CREATE TABLE IF NOT EXISTS pending_user_verifications
+(
+    id      BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    hash    VARCHAR(50) NOT NULL,
+    user_id BIGINT      NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
