66a34169ceaba17a6085d989d080fb8c9dae2822
Tamas Oravecz <oravecztamas@gmail.com>
2024-06-30 23:25:27 +0200

    Created tests (8) for input validation in RegistrationController and UrlController

--- src/main/java/com/example/demo/controllers/UrlController.java
+++ src/main/java/com/example/demo/controllers/UrlController.java
@@ -47,7 +47,6 @@ public class UrlController {
                                 Model model,
                                 RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("urlRequest", urlRequest);
             return "index";
         }
 
